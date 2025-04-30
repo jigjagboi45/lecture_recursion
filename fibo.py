2fc551ba40e2056dab72d62ab61749c2c9f0d2c0
Vojtech Velebny <velebnyvo@gymla.cz>
2025-04-30 14:17:00 +0200

    Recursive fibonacci

--- fibo.py
+++ fibo.py
@@ -1,7 +1,17 @@
 
-def main():
-    pass
+def recursive_nth_fibo(num):
+    # <= misto == je pojistka pred nekonecnou rekurzi
+    if num <= 0:
+        return 0
+    if num == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(num - 1) + recursive_nth_fibo(num - 2)
 
+
+def main():
+    ntc = int(input("Zadejte cislo "))
+    print(f"{ntc}. prvek posloupnosti je {recursive_nth_fibo(ntc)}")
 
 if __name__ == "__main__":
     main()
